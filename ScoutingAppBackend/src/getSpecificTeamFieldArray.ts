afef36c345e94bb491b0b3465f0ab021a01cc8d5
MannyLev <94080012+MannyLev@users.noreply.github.com>
2024-01-13 12:47:21 -0500

    Created a file to get a specific field given a list of JSON files

---
+++ ScoutingAppBackend/src/getSpecificTeamFieldArray.ts
@@ -0,0 +1,22 @@
+function getSpecificTeamFieldArray(scoutInputs: JSON[], field: string) {
+
+    /* const jString = "{\"a\": 1, \"b\": \"str\"}";
+    const jObj = new JSONObject(jString);
+    Object aObj = jObj.get("a");
+    if (aObj instanceof Integer) {
+        do what you want
+    } */
+
+    let returnArray: [] = 
+    for(let i = 0; i < scoutInputs.length, i++;) {
+
+    }
+
+    // What we have to do
+    // First, find the type of the field specified by the string
+    // Second, create an array of scoutInputs.length with type equal to that found in step 1
+    // Third, iterate through the array of jsons and on the next index add that JSON's value for the array
+    // Finally, return the array created
+    // After, create a request system so that the JSON input will be fed in automatically
+    // Could be a request over all matches, over all for a team, a match
+}
